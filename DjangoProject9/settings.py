1c0deffa5c23c484fa59109f2d7c3dec127f4bf6
Yuval-Betito <yuval36811@gmail.com>
2025-01-22 10:41:47 +0200

    Updated settings to use .env for sensitive configurations

--- DjangoProject9/settings.py
+++ DjangoProject9/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,9 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # שרת ה-SMTP של Gmail
-EMAIL_PORT = 587  # פורט TLS
-EMAIL_USE_TLS = True  # השתמש ב-TLS לאבטחה
-EMAIL_HOST_USER = 'communication.ltd001@gmail.com'  # כתובת המייל שלך ב-Gmail
-EMAIL_HOST_PASSWORD = 'stjo bulq lmjd ikvw'  # סיסמת האפליקציה שיצרת
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
